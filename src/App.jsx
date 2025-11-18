83bc30069cd80fc8f20bf2bc0d1ecc6180a8ae49
Mikal Morello <mmorello@hbs.edu>
2025-11-18 18:58:29 -0500

    Initial repo commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Magical Minis</h1>
+        <p>Welcome to magicalminishop.com by Maia and Allie Morello</p>
+      </header>
+      <main>
+        <p>Jack says your website is ready to go!</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
+
